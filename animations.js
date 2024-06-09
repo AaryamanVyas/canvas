19c2f00d2857f1eb58c5fa91d1e3375dfaf8932b
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-06-09 11:23:38 +0000

    made the animation work

--- animations.js
+++ animations.js
@@ -1,16 +1,33 @@
 let canvas = document.getElementById("my_canvas");
-let context = canvas.getcontext("2d")
+let context = canvas.getContext("2d"); // Corrected getcontext to getContext
 
 const speed = 4;
-let position =0;
+let position = 0;
 let movespeed = speed;
 let radius = 40;
 
-function moveball() {
-    if(position + radius > 640) {
-        movespeed = -speed
-    } else if(position -radius < 0){
-        movespeed =speed;
+function moveBall() {
+    if (position + radius > 640) {
+        movespeed = -speed;
+    } else if (position - radius < 0) {
+        movespeed = speed;
     }
     position += movespeed;
 }
+
+function drawBall() {
+    context.clearRect(0, 0, 640, 480);
+    
+    context.fillStyle = "#62687f";
+    context.beginPath(); // Corrected beginpath to beginPath
+    context.arc(position, 50, radius, 0, 2 * Math.PI);
+    context.fill();
+}
+
+function animate() {
+    moveBall();
+    drawBall();
+    window.requestAnimationFrame(animate);
+}
+
+window.requestAnimationFrame(animate);
